1dad580c8654d546d674cd2c7b1e9742c93a9956
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-09-15 00:49:52 +0500

    fix: keep database client available across requests

--- index.js
+++ index.js
@@ -18,17 +18,42 @@ const db = new pg.Client({
 });
 db.connect();
 
-app.get("/", async (req, res) => {
+async function checkVisitedCountries() {
   const result = await db.query("SELECT country_code FROM visitedcountries");
-  console.log(result.rows);
   let countries = [];
   result.rows.forEach((country) => {
     countries.push(country.country_code);
   });
-  console.log(countries);
-  res.render("index.ejs", { countries: countries,total: countries.length });
-  db.end();
+  return countries;
+}
+
+app.get("/", async (req, res) => {
+  const result = await checkVisitedCountries();
+  const countries = result;
+  res.render("index.ejs", { countries: countries, total: countries.length });
 });
+
+//INSERT new country
+app.post("/add", async (req, res) => {
+  const input = req.body["country"];
+
+  const result = await db.query(
+    "SELECT country_code FROM countries WHERE country_name = $1",
+    [input]
+  );
+
+  if (result.rows.length !== 0) {
+    const data = result.rows[0];
+    const countryCode = data.country_code;
+
+    await db.query("INSERT INTO visitedcountries (country_code) VALUES ($1)", [
+      countryCode,
+    ]);
+    res.redirect("/");
+  }
+  // db.end();
+});
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
