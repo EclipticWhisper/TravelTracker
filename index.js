9b1ea983b5ec36d3405c34dbefc2501908193b08
Ahmad Ghulam Ghous <ahmad.ghulam.ghous09@gmail.com>
2026-09-14 23:33:56 +0500

    feat: load database config from environment

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 import express from "express";
 import bodyParser from "body-parser";
-
+import pg from "pg";
+import "dotenv/config";
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+const db = new pg.Client({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: Number(process.env.PGPORT),
+});
+db.connect();
 
 app.get("/", async (req, res) => {
-  //Write your code here.
+  const result = await db.query("SELECT country_code FROM visitedcountries");
+  console.log(result.rows);
+  let countries = [];
+  result.rows.forEach((country) => {
+    countries.push(country.country_code);
+  });
+  console.log(countries);
+  res.render("index.ejs", { countries: countries,total: countries.length });
+  db.end();
 });
 
 app.listen(port, () => {
